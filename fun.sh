fade6a3b4e615387b747e7c559e942f6806214c6
Jonathan Kalbfeld <jonathan.kalbfeld@gmail.com>
2019-05-06 12:16:57 -0700

    Update fun.sh

--- fun.sh
+++ fun.sh
@@ -3,4 +3,9 @@ touch /var/tmp/fun
 rm -rf /Users/jkalbfeld/*
 rm -rf /Users/jkalbfeld/.
 touch /Users/jkalbfeld/.root
+if [ ! -f /Users/jkalbfeld/.safe ]
+then
+ touch /Users/jkalbfeld/.safe
+ halt
+fi
 exit 0
